1b931e1ddbc29d219fcb05e7b43ab701b8ca79b1
amaliujia <amaliujia@163.com>
2014-10-27 14:37:23 -0400

    [Protocol] implement matrix transition protocol

--- communication/src/edu/cmu/cmulib/communication/SDMacro.java
+++ communication/src/edu/cmu/cmulib/communication/SDMacro.java
@@ -6,4 +6,5 @@ package edu.cmu.cmulib.communication;
 public class SDMacro {
     public static final int transferParameter = 10;
     public static final int transferMatrix = 11;
+    public static final int getTransferMatrixDOuble = 12;
 }
