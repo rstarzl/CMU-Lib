3eed936f5873314c2765e504fdd6d5f5035c3ee7
qqsi <taog@andrew.cmu.edu>
2015-03-03 21:07:05 -0500

    finished append API

--- API/src/main/edu/cmu/cmulib/API/data/DataAPI.java
+++ API/src/main/edu/cmu/cmulib/API/data/DataAPI.java
@@ -1,16 +1,18 @@
 package edu.cmu.cmulib.API.data;
 
+import cmu.core.Mat;
+
 /**
  * a DataAPI used to read file and pass it to the Master Node Sample Usage:
  * 
- * Example:
- *       DataAPI processor = new DataAPI();
- *       
- *       IgnoreRearTokensStrategy moreStra = new IgnoreRearTokensStrategy();
- *       PaddingRearWithValueStrategy lessStra = new PaddingRearWithValueStrategy("9.9");
- *       processor.setNotEnoughDelimiterStrategy(lessStra);
- *       processor.setTooManyDelimiterErrorStrategy(moreStra);
- *       processor.processingData(1000, 100, fileName, ",", "dataType");
+ * Example: DataAPI processor = new DataAPI();
+ * 
+ * IgnoreRearTokensStrategy moreStra = new IgnoreRearTokensStrategy();
+ * PaddingRearWithValueStrategy lessStra = new
+ * PaddingRearWithValueStrategy("9.9");
+ * processor.setNotEnoughDelimiterStrategy(lessStra);
+ * processor.setTooManyDelimiterErrorStrategy(moreStra);
+ * processor.processingData(1000, 100, fileName, ",", "dataType");
  * 
  * @author Cambi
  */
@@ -95,10 +97,33 @@ public class DataAPI {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         // Sends data to Master's acceptData method
         return (Master.acceptData(numSlaves, tokens, numOfRows, numOfColumns,
                 dataType));
 
+    }
+
+    /**
+     * Appending data to Master Node
+     * 
+     * @param mat
+     *            the matrix to be appended
+     * @return true if the appending is successful
+     */
+    public boolean appendingData(String[][] mat) {
+        return Master.appendData(mat);
+
+    }
+
+    /**
+     * Deleting data from the end
+     * 
+     * @param numOfItems
+     *            number of items to be deleted
+     * @return true if deletion is successful
+     */
+    public boolean deletingData(int numOfItems) {
+        return Master.deleteData(numOfItems);
     }
 }
